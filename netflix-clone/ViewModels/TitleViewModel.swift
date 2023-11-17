6ac809b2832a7192971461308097fe5b314aa303
Batanai <batiemc@gmail.com>
2023-11-17 17:40:59 +0200

    Upcoming screen complete

---
+++ netflix-clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  netflix-clone
+//
+//  Created by MacBook on 2023/11/16.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
